dbf76909c1f9fec9224f54a084b2f45274f051ca
Kirill <darthrozwell@gmail.com>
2026-08-24 14:39:47 +0300

    ignore

--- backend/src/database_core.py
+++ backend/src/database_core.py
@@ -1,10 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from src.models_core import Base
+from src.settings import settings
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:6432/postgres"
 
-engine = create_async_engine(url=DATABASE_URL)
+engine = create_async_engine(url=settings.database_url, echo=settings.debug)
 
 new_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
